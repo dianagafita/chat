dc8edbce902e669b73540c4de2e8f7efd1579d4a
Gafita Diana <diana.gafita@student.utcluj.ro>
2023-01-12 23:21:20 +0200

    d

---
+++ src/Home/index.js
@@ -0,0 +1,19 @@
+
+import SupportEngine from '../SupportEngine';
+
+
+function Home() {
+    return (
+        <div>
+
+         
+            
+            <h1>Hello User</h1>
+
+            <SupportEngine />
+
+        </div>
+    )
+}
+
+export default Home
